b53e4b5fa46a0e884ffc2e9ba50a267c2c722531
Ankit Kayastha <ankit.kayastha@duke.edu>
2016-11-09 01:30:47 -0500

    Aws email (#7) - made by Ankit Kayastha
    * Trying AWS Email
    
    * Edits to SES client
    
    * throw new exception and changed array to list

---
+++ src/main/java/utilities/aws/SESClient.java
@@ -0,0 +1,38 @@
+package utilities.aws;
+
+import com.amazonaws.regions.Region;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.simpleemail.*;
+import com.amazonaws.services.simpleemail.model.*;
+
+import java.util.List;
+
+public class SESClient extends AWSClient {
+    private AmazonSimpleEmailServiceClient client;
+    private static final String FROM = property.getProperty("source.email");
+
+    public SESClient() {
+        this.client = new AmazonSimpleEmailServiceClient();
+        Region REGION = Region.getRegion(Regions.US_WEST_2);
+        this.client.setRegion(REGION);
+    }
+
+    public void verifyEmail(String emailAddress) {
+        VerifyEmailIdentityRequest identityRequest = new VerifyEmailIdentityRequest();
+        this.client.verifyEmailIdentity(identityRequest.withEmailAddress(emailAddress));
+    }
+    public void sendEmail(List<String> toAddresses, String subjectLine, String emailMessage) throws Exception {
+        Destination destination = new Destination().withToAddresses(toAddresses);
+        Content subject = new Content().withData(subjectLine);
+        Content content = new Content().withData(emailMessage);
+        Body body = new Body().withText(content);
+        Message message = new Message().withSubject(subject).withBody(body);
+        SendEmailRequest request = new SendEmailRequest().withSource(this.FROM).withDestination(destination).withMessage(message);
+        try{
+            client.sendEmail(request);
+        }
+        catch (Exception exception) {
+            throw new Exception("Email could not be sent");
+        }
+    }
+}
