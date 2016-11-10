63ab6c79cd494995829378447d165bcb12d410e7
Sivaneshwaran Loganathan <sivaneshwaran.loganathan@duke.edu>
2016-11-10 03:05:49 -0500

    completed the thymeleaf and tested

--- src/main/java/lostandfound/founditem/FoundItemService.java
+++ src/main/java/lostandfound/founditem/FoundItemService.java
@@ -2,12 +2,17 @@ package lostandfound.founditem;
 
 import data.FoundItem;
 import data.LostItem;
+import data.User;
 import lostandfound.requestmodels.FoundItemRequest;
 import lostandfound.std.Service;
 import lostandfound.std.models.StdResponse;
 import lostandfound.std.models.StdResponseWithBody;
+import utilities.aws.SESClient;
 
 import java.util.List;
+import java.util.Locale;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @org.springframework.stereotype.Service
 public class FoundItemService extends Service {
@@ -18,6 +23,7 @@ public class FoundItemService extends Service {
     }
 
     public StdResponse createFoundItem(FoundItemRequest request) {
+        //TODO check to see whether this method can take in the picture and handle the S3
         FoundItem foundItem = new FoundItem(request.geolocation, request.timestamp, request.uniqueId, request.tags);
         int id = itemAccessor.commitFoundItemWithTags(foundItem);
         itemAccessor.getAllLostItemsWithTags().parallelStream()
@@ -27,7 +33,16 @@ public class FoundItemService extends Service {
     }
 
     private void sendItemToLostPerson(LostItem lostItem, FoundItem foundItem) {
-        //TODO fill up email stuff later
+        User lostUser = authAccessor.getUserByUniqueID(lostItem.uniqueId);
+        User foundUser = authAccessor.getUserByUniqueID(foundItem.uniqueId);
+        String emailHTML = emailTemplateClient.createEmail(lostUser.name,foundUser.email,foundUser.name,foundItem.pictureURL,new Locale("US"));
+        try{
+            sesClient.sendEmail(Stream.of(lostUser.email).collect(Collectors.toList()),"Your Lost Item Is Found",emailHTML);
+        }
+        catch (Exception e){
+            //TODO enable logging in the future but for now :-
+            e.printStackTrace();
+        }
     }
 
 
