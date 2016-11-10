f7531be74c9505a7c5f0f63fa3090afcd1d56853
Sivaneshwaran Loganathan <sivaneshwaran.loganathan@duke.edu>
2016-11-10 03:05:49 -0500

    completed the thymeleaf and tested
    added a better exception name

--- src/main/java/utilities/ThymeleafEmailClient.java
+++ src/main/java/utilities/ThymeleafEmailClient.java
@@ -1,16 +1,46 @@
 package utilities;
 
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
 import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
-import org.thymeleaf.templateresolver.TemplateResolver;
+import org.thymeleaf.templateresolver.ITemplateResolver;
+
+import java.util.Locale;
 
 public class ThymeleafEmailClient {
+    public static final String EMAIL_TEMPLATE_ENCODING = "UTF-8";
 
-    private TemplateResolver emailTemplateResolver() {
-        TemplateResolver templateResolver = new ClassLoaderTemplateResolver();
-        templateResolver.setPrefix("/mail/");
-        templateResolver.setTemplateMode("HTML5");
-        templateResolver.setOrder(1);
+    private TemplateEngine templateEngine;
+
+    public ThymeleafEmailClient(){
+        this.templateEngine = new TemplateEngine();
+        this.templateEngine.addTemplateResolver(htmlTemplateResolver());
+    }
+
+    /**
+     * THYMELEAF: Template Resolver for email templates.
+     */
+    private ITemplateResolver htmlTemplateResolver() {
+        ClassLoaderTemplateResolver templateResolver = new ClassLoaderTemplateResolver();
+        templateResolver.setSuffix(".html");
+        templateResolver.setCharacterEncoding(EMAIL_TEMPLATE_ENCODING);
+        templateResolver.setCacheable(false);
         return templateResolver;
+    }
+
+    public String createEmail(String recipientName, String foundEmailAddress, String foundPerson, String imageResourceName, Locale locale) {
+        // Prepare the evaluation context
+        final Context ctx = new Context(locale);
+        ctx.setVariable("lostName", recipientName);
+        ctx.setVariable("emailAdd", foundEmailAddress);
+        ctx.setVariable("foundName", foundPerson);
+        ctx.setVariable("imageResourceName", imageResourceName); // so that we can reference it from HTML
+
+        //TODO remove the cid for emails :/
+
+        // Create the HTML body using Thymeleaf
+        final String htmlContent = this.templateEngine.process("email_template", ctx);
+        return htmlContent;
     }
 
 }
