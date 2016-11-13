ac85bd05a3d57a363973c317af7ddc609165e0af
James Mosca <jcm73@duke.edu>
2016-11-12 19:11:50 -0500

    Oauth (#16)
    * Add UI to login and redirect to oauth
    
    * Update SQL file for authtoken and fix thymeleaf bug
    
    * Logging in now retrieves the user's netid with an API call
    
    * auto changes
    
    * changed auth_token to text

--- src/main/java/lostandfound/MainController.java
+++ src/main/java/lostandfound/MainController.java
@@ -8,13 +8,18 @@ import data.LostItem;
 import data.User;
 import data.accessors.AuthAccessor;
 import data.accessors.ItemAccessor;
+import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Arrays;
+
+import lostandfound.exceptions.AuthException;
 
 
 /**
@@ -56,23 +61,54 @@ public class MainController {
     @RequestMapping("/loginsuccess")
     public String loginSuccess(@RequestParam String access_token) {
         /*
-        This method will get the access token and redirect to front end
+        This method will get the access token then request netID from colab API
+        Then redirects to welcome page
          */
-        return "Login Succeeded: " + access_token;
+        RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.add("Authorization", "Bearer " + access_token);
+        headers.add("x-api-key", "lost-and-found");
+
+        HttpEntity<String> entity = new HttpEntity<String>(headers);
+        String apiURL = "https://api.colab.duke.edu/identity/v1/";
+
+        ResponseEntity<String> response = restTemplate.exchange(apiURL, HttpMethod.GET, entity, String.class);
+        String[] bodyArr = response.getBody().split(",");
+        String netID = "";
+        for (int i = 0; i < bodyArr.length; i++) {
+            if (bodyArr[i].contains("netid")) {
+                String[] association = bodyArr[i].split(":");
+                netID = association[1].trim().replace("\"", "");
+            }
+        }
+        if (netID.equals("")) {
+            throw new AuthException("A valid netid could not be found");
+        }
+        if (response.getStatusCodeValue() != 200) {
+            throw new AuthException("The request for the netID was bad");
+        }
+        return netID;
     }
 
     @RequestMapping("/authconfirm")
-    public String authconfirm(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
-        model.addAttribute("name", name);
+    public String authconfirm(@RequestParam(value="name", required=false, defaultValue="World") String name) {
         return "authorize";
     }
 
-    @RequestMapping("/loginfailure")
-    public String loginFailure() {
-        /*
-        This method will get the access token and redirect to front end
-         */
+    @RequestMapping(value = "/loginfailure")
+    public String loginFailure(@RequestParam(value="status", required=false, defaultValue="World") String status, Model model) {
+        model.addAttribute("status", status);
         return "index";
+    }
+
+    @RequestMapping("/loginwelcome")
+    /*
+    Show the welcome/home page
+     */
+    public String loginwelcome(@RequestParam(value="netid", required=false, defaultValue="NOT FOUND") String netid) {
+        return "loginwelcome";
+
     }
 
 }
