8558759e2c58b7b7a0ad3443293a94ac4fa09ba1
Sivaneshwaran Loganathan <sivaneshwaran.loganathan@duke.edu>
2016-11-15 14:40:27 -0500

    added the lost and found data accessor and created endpoints (#17)
    added the markItemRequest feature to mark item as lost or/and found

--- src/main/java/data/FoundItem.java
+++ src/main/java/data/FoundItem.java
@@ -11,6 +11,10 @@ public class FoundItem extends Item {
         this.lostId = lostId;
     }
 
+    public FoundItem(Integer id, String geolocation, Timestamp timestamp, String uniqueID, List<String> tags, String pictureURL) {
+        super(id,geolocation,timestamp,uniqueID,tags,pictureURL);
+    }
+
     public FoundItem(String geolocation, Timestamp timestamp, String uniqueID, List<String> tags){
         super(geolocation,timestamp,uniqueID,tags);
     }
--- src/main/java/lostandfound/founditem/FoundItemController.java
+++ src/main/java/lostandfound/founditem/FoundItemController.java
@@ -1,6 +1,7 @@
 package lostandfound.founditem;
 
 import lostandfound.requestmodels.FoundItemRequest;
+import lostandfound.requestmodels.MarkItemRequest;
 import lostandfound.std.Controller;
 import lostandfound.std.models.StdRequest;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,6 +27,20 @@ public class FoundItemController extends Controller {
     public ResponseEntity add(@RequestBody FoundItemRequest request){
         pre(request);
         return wrap(foundItemService.createFoundItem(request));
+    }
+
+    @RequestMapping(value = "/allItemsByUser/{uniqueId}", method = RequestMethod.GET)
+    public ResponseEntity getFoundItems(@PathVariable String uniqueId){
+        //TODO request method when the frontend is built so auth can happen
+        //pre(request);
+        return wrap(foundItemService.getAllFoundItemsWithTags(uniqueId));
+    }
+
+    @RequestMapping(value = "/markItem", method = RequestMethod.POST, headers = {"Content-type=application/json"})
+    @ResponseBody
+    public ResponseEntity mark(@RequestBody MarkItemRequest request){
+        pre(request);
+        return wrap(foundItemService.markItemAsFound(request));
     }
 
 
