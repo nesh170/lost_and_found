cdf63f691ac4fd0e5b2ca02fdc941d95d700809a
Sivaneshwaran Loganathan <sivaneshwaran.loganathan@duke.edu>
2016-11-22 16:45:52 -0500

    refactored mark

--- src/main/java/data/accessors/ItemAccessor.java
+++ src/main/java/data/accessors/ItemAccessor.java
@@ -10,8 +10,8 @@ public class ItemAccessor extends Accessor {
     }
 
     public boolean markItemAsFound(int foundId, int lostId) {
-        int lostSuccess = myContext.update(LOST_ITEMS).set(LOST_ITEMS.FOUND_ID, foundId).where(LOST_ITEMS.ID.equal(lostId)).execute();
-        int foundSuccess = myContext.update(FOUND_ITEMS).set(FOUND_ITEMS.LOST_ID, lostId).where(FOUND_ITEMS.ID.equal(foundId)).execute();
+        int lostSuccess = (lostId == -1) ? -1 : myContext.update(LOST_ITEMS).set(LOST_ITEMS.FOUND_ID, foundId).where(LOST_ITEMS.ID.equal(lostId)).execute();
+        int foundSuccess = (foundId == -1) ? -1 : myContext.update(FOUND_ITEMS).set(FOUND_ITEMS.LOST_ID, lostId).where(FOUND_ITEMS.ID.equal(foundId)).execute();
         return lostSuccess == 1 || foundSuccess == 1;
     }
 
