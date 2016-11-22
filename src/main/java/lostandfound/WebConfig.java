0b477013e11a5db1da7fef4f5640f6388b231798
Sivaneshwaran Loganathan <sivaneshwaran.loganathan@duke.edu>
2016-11-22 18:50:10 -0500

    Cors fix (#23)
    * fixing the cors
    
    * fixed the cors issue

--- src/main/java/lostandfound/WebConfig.java
+++ src/main/java/lostandfound/WebConfig.java
@@ -2,11 +2,9 @@ package lostandfound;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
-@EnableWebMvc
 public class WebConfig extends WebMvcConfigurerAdapter {
 
     @Override
